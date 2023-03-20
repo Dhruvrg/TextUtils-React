47389d642c98e2a6549f1cea754a864ea42da14b
Dhruv <dhruvrg2003@gmail.com>
2023-03-20 08:32:21 -0700

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,9 +25,9 @@ function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/from">
+                {/* <a className="nav-link active" aria-current="page" href="/from">
                   Home
-                </a>
+                </a> */}
               </li>
               {/* <li className="nav-item">
                 <a className="nav-link" href="/about">
